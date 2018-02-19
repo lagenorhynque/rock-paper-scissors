69b92c777864abf750a6cd18934902a652dde51d
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-02-19 21:46:03 +0900

    implement rock-paper-scissors

--- src/cljs/rock_paper_scissors/rps.cljs
+++ src/cljs/rock_paper_scissors/rps.cljs
@@ -1,1 +1,24 @@
 (ns rock-paper-scissors.rps)
+
+(defn <-hand [hand]
+  (case hand
+    ::rock 0
+    ::scissors 1
+    ::paper 2))
+
+(defn ->hand [num]
+  (case num
+    0 ::rock
+    1 ::scissors
+    2 ::paper))
+
+(defn fight [you enemy]
+  (letfn [(result [n]
+            (case n
+              0 ::draw
+              1 ::lose
+              2 ::win))]
+    (-> (- (<-hand you) (<-hand enemy))
+        (+ 3)
+        (mod 3)
+        result)))
