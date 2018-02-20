e6dea82873cec7a62484d06a3768179b0665ab93
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-02-20 19:40:41 +0900

    define events

--- src/cljs/rock_paper_scissors/events.cljs
+++ src/cljs/rock_paper_scissors/events.cljs
@@ -3,6 +3,18 @@
             [rock-paper-scissors.db :as db]))
 
 (re-frame/reg-event-db
- ::initialize-db
- (fn  [_ _]
-   db/default-db))
+  ::initialize-db
+  (fn  [_ _]
+    db/default-db))
+
+(re-frame/reg-event-db
+  ::next-game
+  (fn [db _]
+    (assoc db :scene ::db/now-playing)))
+
+(re-frame/reg-event-db
+  ::select-your-hand
+  (fn [db [_ h]]
+    (assoc db
+           :you h
+           :scene ::db/over)))
