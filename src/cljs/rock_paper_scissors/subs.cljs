cde71a2e10e39bb4aa6a96a05c45840ea6853fb1
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2018-02-21 00:35:39 +0900

    refactor view & subscriptions

--- src/cljs/rock_paper_scissors/subs.cljs
+++ src/cljs/rock_paper_scissors/subs.cljs
@@ -1,5 +1,6 @@
 (ns rock-paper-scissors.subs
-  (:require [re-frame.core :as re-frame]))
+  (:require [re-frame.core :as re-frame]
+            [rock-paper-scissors.rps :as rps]))
 
 (re-frame/reg-sub
   ::scene
@@ -10,3 +11,24 @@
   ::you-enemy
   (fn [db _]
     (select-keys db [:you :enemy])))
+
+(re-frame/reg-sub
+  ::you-enemy-hands
+  :<- [::you-enemy]
+  (fn [{:keys [you enemy]} _]
+    (str (name you) "(YOU) VS " (name enemy) "(ENEMY)")))
+
+(re-frame/reg-sub
+  ::fight-result
+  :<- [::you-enemy]
+  (fn [{:keys [you enemy]} _]
+    (rps/fight you enemy)))
+
+(re-frame/reg-sub
+  ::result-color
+  :<- [::fight-result]
+  (fn [r _]
+    (case r
+      ::rps/win "red"
+      ::rps/lose "blue"
+      ::rps/draw "gray")))
--- src/cljs/rock_paper_scissors/views.cljs
+++ src/cljs/rock_paper_scissors/views.cljs
@@ -5,6 +5,11 @@
             [rock-paper-scissors.rps :as rps]
             [rock-paper-scissors.subs :as subs]))
 
+(defn start-button [label]
+  [:input {:type "button"
+           :on-click #(re-frame/dispatch [::events/next-game])
+           :value label}])
+
 (defn hands []
   [:div (map (fn [h]
                ^{:key h}
@@ -13,27 +18,16 @@
                         :value h}])
              [::rps/rock ::rps/paper ::rps/scissors])])
 
-(defn result [you enemy]
-  (let [r (rps/fight you enemy)]
-    (case r
-      ::rps/win [:h1 {:style {:color "red"}}
-                 r]
-      ::rps/lose [:h1 {:style {:color "blue"}}
-                  r]
-      ::rps/draw [:h1 {:style {:color "gray"}}
-                  r])))
+(defn result []
+  (let [r @(re-frame/subscribe [::subs/fight-result])]
+    [:h1 {:style {:color @(re-frame/subscribe [::subs/result-color])}}
+     r]))
 
 (defn main-panel []
-  (let [scene @(re-frame/subscribe [::subs/scene])]
-    (case scene
-      ::db/start [:input {:type "button"
-                          :on-click #(re-frame/dispatch [::events/next-game])
-                          :value "Game Start"}]
-      ::db/now-playing [hands]
-      ::db/over (let [{:keys [you enemy]} @(re-frame/subscribe [::subs/you-enemy])]
-                  [:div
-                   [:h1 (str (name you) "(YOU) VS " (name enemy) "(ENEMY)")]
-                   [result you enemy]
-                   [:input {:type "button"
-                            :on-click #(re-frame/dispatch [::events/next-game])
-                            :value "Next Game"}]]))))
+  (case @(re-frame/subscribe [::subs/scene])
+    ::db/start [start-button "Game Start"]
+    ::db/now-playing [hands]
+    ::db/over [:div
+               [:h1 @(re-frame/subscribe [::subs/you-enemy-hands])]
+               [result]
+               [start-button "Next Game"]]))
